3080a7d7065a3adfdf8d2f95002786784973ca39
OmarGonD <oma.gonzales@gmail.com>
2015-07-16 08:02:51 -0500

    16/07/2015 Cambios. Weekdays es el principal cambio. Aún no logro reemplazar los NAs por el mean según intervalos.

--- Assgiement1.R
+++ Assgiement1.R
@@ -14,8 +14,8 @@ activity <- read.csv("Z:\\OMAR\\RCoursera\\RepData\\RepData_PeerAssessment1\\act
 activity$date <- as.Date(activity$date)
 
 
-View(activity)
-str(activity)
+#View(activity)
+#str(activity)
 
 
 
@@ -30,8 +30,8 @@ steps_bydate <- activity %>%
 
 
 
-head(steps_bydate)
-tail(steps_bydate)
+#head(steps_bydate)
+#tail(steps_bydate)
 
 
 ###########
@@ -42,9 +42,9 @@ ggplot(data=steps_bydate, aes(steps_bydate$steps_sum)) + geom_histogram()
 
 #######
 
-mean(steps_bydate$steps_sum, na.rm = T)
-median(steps_bydate$steps_sum, na.rm = T)
-sum(steps_bydate$steps_sum, na.rm = T)
+#mean(steps_bydate$steps_sum, na.rm = T)
+#median(steps_bydate$steps_sum, na.rm = T)
+#sum(steps_bydate$steps_sum, na.rm = T)
 
 
 
@@ -70,13 +70,68 @@ ggplot(data=activity_pattern, aes(x = activity_pattern$interval, y = activity_pa
 
 # Need the number of rows per interval...
 
-head(activity_pattern)
-tail(activity_pattern)
-View(activity_pattern)
+#head(activity_pattern)
+#tail(activity_pattern)
+#View(activity_pattern)
+
+
+
+
+
+## Rango con max numero de Steps.
+
+activity_pattern[activity_pattern$steps_sum == max(activity_pattern$steps_sum),]
+
+activity_pattern[activity_pattern$steps_mean == max(activity_pattern$steps_mean),]
+
+
+
+## Parte 4
+## Imputing missing values:
 
 
 # Total number of NAs in dataset.
 sum(is.na(activity))
+
+## 
+
+activity_complete <- activity
+
+activity_complete$steps <- activity_pattern$steps_mean[match(any(activity_complete$interval), activity_pattern$interval)]
+
+#activity_complete$steps[is.na(activity_complete$steps)] <- "omar"
+
+
+if (any(activity_complete$steps) == T){
+    activity_complete$steps <- activity_pattern$steps_mean[match(activity_complete$interval, activity_pattern$interval)]
+}
+
+
+
+
+
+dput(activity_complete, "df1")
+dput(activity_pattern, "df2")
+
+
+## Parte 5 ##
+
+activity$days <- weekdays(activity$date)
+
+
+
+    
+activity <- mutate(activity,
+                   Weekday = NA,
+                   Weekday = ifelse(grepl("lunes", days)
+                                    | grepl("martes", days) | grepl("miércoles", days)
+                                    | grepl("jueves", days) | grepl("viernes", days),
+                                    "Weekday", Weekday),
+                             
+                   Weekday = ifelse(grepl("sábado", days)
+                                   | grepl("domingo", days),
+                                   "Weekend", Weekday))
+
 
 
 
